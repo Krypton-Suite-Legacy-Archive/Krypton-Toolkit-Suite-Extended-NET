e24bd1f2f8ba6d79a9a99521ba21e6461da3b80f
Peter Wagner <peterwagner@live.co.uk>
2020-02-11 18:00:26 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Shared/Toolkit Settings/Settings/Core/ColourIntegerSettings.Designer.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Settings/Settings/Core/ColourIntegerSettings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ToolkitSettings.Core {
+namespace ToolkitSettings.Settings.Core {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
